c13921a86d3ea5e72b164dbc7e04a4bca969cb6c
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2025-04-05 18:26:37 -0300

    Update nome.py

--- cogs/nome.py
+++ cogs/nome.py
@@ -76,7 +76,7 @@ class NomeNoCanalCog(commands.Cog):
             ),
             color=COR_PADRAO
         )
-        embed_pedido.set_image(url="https://i.imgur.com/GI1TZEa.gif")  # <- GIF adicionado aqui
+        embed_pedido.set_image(url="https://imgur.com/a/GI1TZEa.gif")  # <- GIF adicionado aqui
         embed_pedido.set_footer(text="Sistema de Verificação")
 
         pedido_msg = await channel.send(embed=embed_pedido)
